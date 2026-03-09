be20c00a355ff20868973471bb8045b201a0526e
Raffel222 <Raffel222@gmail.com>
2026-03-10 00:23:24 +0100

    feat: wire App to userStore and chat API

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import Detail from "./components/detail/Detail";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import "react-toastify/dist/ReactToastify.css";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./lib/firebase";
 import { useEffect } from "react";
 import { useUserStore } from "./lib/userStore";
 import { useChatStore } from "./lib/chatStore";
-
+import { hasToken } from "./lib/api/client";
 
 function App() {
 
-  const {user: currentUser, isLoading, fetchUser} = useUserStore();
-  const {chatId} = useChatStore();
+  const { user: currentUser, isLoading, fetchUser } = useUserStore();
+  const { chatId } = useChatStore();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      fetchUser(user?.uid || "");
-    });
-    return () => unsubscribe();
+    if (hasToken()) {
+      fetchUser("me");
+    } else {
+      fetchUser("");
+    }
   }, [fetchUser]);
 
   if (isLoading) return <div className="loading">Loading...</div>;
